035ffb7ec7091a525f1fd739c64770e47bd4e39f
Sergi Biosca Beneyto <serbidaw@gmail.com>
2023-03-21 20:36:19 +0100

    Actualizar settings.py

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)czav-+xg#y$vxu$otto-m8l87q#n87r-5@xbx9opu=lfl3#j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
