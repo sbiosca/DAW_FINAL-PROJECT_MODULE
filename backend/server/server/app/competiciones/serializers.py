d5e22dfac47357fb85cfeab87e4160f5d608ddde
Sergi Biosca <serbidaw@gmail.com>
2023-03-28 18:52:25 +0200

    Add all folders to put tables modules django

---
+++ backend/server/server/app/competiciones/serializers.py
@@ -0,0 +1,24 @@
+
+from pyexpat import model
+from rest_framework import serializers
+from .models import Equipo
+from django.core.serializers import serialize
+
+class EquipoSerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Equipo
+            fields = ('__all__')
+            #fields = ('id', 'name', 'lat', 'long')
+    
+    def to_Equipo(instance):
+        return {
+            'id': instance.id,
+            'name': instance.name,
+            'city': instance.city,
+            'stadium': instance.stadium,
+            'lat': instance.lat,
+            'long': instance.long,
+            'shield': instance.shield
+        }
+        
+        
